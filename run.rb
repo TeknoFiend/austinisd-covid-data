7646a6f2a20ce8779bc54a7a03b2112bb21eeb59
Weston Sewell <weston.sewell@gmail.com>
2020-11-15 16:40:14 -0600

    Make script poll every 8 hours, fix date -> filename conversion

--- run.rb
+++ run.rb
@@ -2,36 +2,49 @@ require "open-uri"
 require "json"
 require 'nokogiri'
 
-data = URI.parse("https://www.austinisd.org/covid-dashboard/ajax?_wrapper_format=drupal_ajax").read
+REFRESH_INTERVAL_HOURS = 8
+MINUTES_PER_HOUR = 60
+SECONDS_PER_MINUTE = 60
 
-html = JSON.parse(data)[0]['data']
+while(true) do
+  puts "Downloading at #{Time.now}"
+  puts '----------------------------------------'
+  data = URI.parse("https://www.austinisd.org/covid-dashboard/ajax?_wrapper_format=drupal_ajax").read
 
-$fragment = Nokogiri::HTML.fragment(html)
+  html = JSON.parse(data)[0]['data']
 
-if $fragment.search('thead').first.text.strip.gsub(/\s+/,',') != 'School,Total,New,Cases,Total,Cumulative,Cases,Total,New,Exposures,Total,Cumulative,Exposures'
-  raise StandardError.new('Error: data format changed')
+  $fragment = Nokogiri::HTML.fragment(html)
+
+  if $fragment.search('thead').first.text.strip.gsub(/\s+/,',') != 'School,Total,New,Cases,Total,Cumulative,Cases,Total,New,Exposures,Total,Cumulative,Exposures'
+    raise StandardError.new('Error: data format changed')
+  end
+
+  $fragment.text =~ /updated (\d+\/\d+\/\d+)/i
+  unless( date = $1 )
+    raise StandardError.new("Error: couldn't find update date")
+  end
+
+  month,day,year = date.strip.split('/').map(&:strip).map(&:to_i)
+  year = (year + 2000) if year < 100
+  updated_at = Date.new(year,month,day)
+
+  table = $fragment.search('tbody')
+  rows = table.search('tr')
+
+  data = rows.map do |row|
+    col_data = row.search('td').map { |cell| cell.text.strip }.join(',')
+  end
+
+  csv = "School,Total New Cases,Total Cumulative Cases,Total New Exposures,Total Cumulative Exposures\n" + data.join("\n")
+
+  filename = "data/#{updated_at.strftime('%Y-%m-%d')}.csv"
+  File.open(filename, 'w') { |file| file.write(csv) }
+
+  `git add #{filename}`
+  `git commit #{filename} -m "Data for #{updated_at}"`
+  `git push`
+
+  puts "Updated #{filename}"
+  puts '#################################################'
+  sleep REFRESH_INTERVAL_HOURS * MINUTES_PER_HOUR * SECONDS_PER_MINUTE
 end
-
-$fragment.text =~ /updated (\d+\/\d+\/\d+)/i
-unless( date = $1 )
-  raise StandardError.new("Error: couldn't find update date")
-end
-
-month,day,year = date.strip.split('/').map(&:strip).map(&:to_i)
-updated_at = Date.new(year+2000,month,day)
-
-table = $fragment.search('tbody')
-rows = table.search('tr')
-
-data = rows.map do |row|
-  col_data = row.search('td').map { |cell| cell.text.strip }.join(',')
-end
-
-csv = "School,Total New Cases,Total Cumulative Cases,Total New Exposures,Total Cumulative Exposures\n" + data.join("\n")
-
-filename = "#{updated_at.strftime('%Y-%m-%d')}.csv"
-File.open(filename, 'w') { |file| file.write(csv) }
-
-`git add #{filename}`
-`git commit #{filename} -m "Data for #{updated_at}"`
-`git push`
